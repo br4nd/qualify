85256da11242e69c3b4968b63111648077ec8b66
br4nd <brandondewberry@gmail.com>
2016-03-16 09:36:43 -0500

    Version 3 changes

--- qualify.py
+++ qualify.py
@@ -12,11 +12,17 @@ creates <inputfile>.pkl and <inputfile>.pdf
 Originally created Feb 2016
 @author: B. Dewberry
 Feb 21 2016: changed name to qualify.py, added qualify-info.csv
-Mar 2016 :
+Version 2, Mar 5 2016 :
 - use qualify-table.csv from same folder as data
 - print every 100th line processed in logfile (faster)
 - Alan's enhancements: snr biases, DUT TX SNR plot
 - FigSize specified in qualify-table.csv
+Version 3, Mar 14 2016:
+- prints control file parms in pdf
+- added TimeLimit (minutes) support in qualify-table.csv (see example in TestData subfolder)
+- fixed DUT RX SNR label
+- removed save to pickle (turns out csv read is just as fast, and just as compact)
+- removed call to lib.read_log and lib.query_file (now self-contained - doesn't require lib)
 """
 
 import argparse
@@ -30,8 +36,8 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 from pprint import pprint as pp
 
-sys.path.append('..')
-import lib
+#sys.path.append('..')
+#import lib
 
 parser = argparse.ArgumentParser(description='Radio Qualification logfile analysis')
 parser.add_argument('-f','--ffn_in',help='input file')
@@ -41,122 +47,139 @@ myargs = parser.parse_args()
 ffn_in = myargs.ffn_in
 compress_flag = myargs.compress
 
-#ffn_in = 'test-data/test run = 10 pii 975 864002.pkl'
-
 # temporary override
-#ffn_in = '/Volumes/Transcend/Data/IRAP/test run = 10 pii 975 864002.pkl'
-#ffn_in = '/media/brandon/C0E5-8BEE/Data/Qualify items from Alan/Enhancements/Dry run (with resp snr)006.csv'
 #ffn_in = '/media/brandon/C0E5-8BEE/Data/Qualify items from Alan/Enhancements/Dry run (with resp snr)006.pkl'
+#ffn_in = './TestData/Dry run - rsp snr - short.csv'
 
-
-#------- Read logfile and pickle it
-#ffn_in = './test-data/test run = 10 pii 975 864002.pkl'
+#------- Query logfile name
 if not ffn_in :
-    ffn_in = lib.query_file()
+    import Tkinter, tkFileDialog
+    prompt = 'Choose an input file'
+    types = [('All files','*'),('csv','*.csv'),('pkl','*.pkl')]
+    defaultextension = '*'
+    root = Tkinter.Tk()
+    root.withdraw() # don't want a full GUI
+    root.update()
+    ffn_in = tkFileDialog.askopenfilename(parent=root,title=prompt,filetypes=types)
+#    ffn_in = lib.query_file()
 
 path,fn_in = os.path.split(ffn_in)
 fn_main,fn_ext = os.path.splitext(fn_in)
 fn_pkl = fn_main + '.pkl'
 ffn_pkl = os.path.join(path,fn_pkl)
 
-if fn_ext == '.csv' :
-    #if not os.path.isfile(ffn_pkl) or compress_flag :
-    D = lib.read_log(ffn_in)
-    fh_pkl = open(ffn_pkl,'wb')
-    pickle.dump(D,fh_pkl)
-    fh_pkl.close()
-    print 'Compressed data saved to ', ffn_pkl
-else :
-    D = pickle.load(open(ffn_pkl,'rb'))
+fn_pdf = fn_main + '.pdf'
+ffn_pdf = os.path.join(path,fn_pdf)
+pdf = PdfPages(ffn_pdf)
 
-#------------- read qualify table
+#------------- read qualify parameter table
 import csv
 pii_table = []
 snr_bias_table = []
 qualify_table_ffn = os.path.join(path,'qualify-table.csv')
 with open(qualify_table_ffn,'rb') as csvfile :
     #    stuff = csv.reader(csvfile,delimiter=' ',quotechar='|')
+    plt.ion()
+    fig,ax = plt.subplots()
     stuff = csv.reader(csvfile)
+    fig_y = 0.95
+    fig_y_snrbias = 0.95
+    fontsize = 12
     for row in stuff :
         print ', '.join(row)
+        if row[0] == '' :
+            continue
         if 'FigSize' == row[0] :
             fig_size = (int(row[1]),int(row[2]))
+            plt.text(0.05,fig_y,'%s: %s, %s' % (row[0],row[1],row[2]),fontsize=fontsize)
         elif 'Pii' == row[0][:3] :
             pii_row = (row[0], row[1], row[2], row[3].lstrip(' '))
             pii_table.append(pii_row)
+            plt.text(0.05,fig_y,'%s: %s, %s, %s' % (row[0],row[1],row[2],row[3]),fontsize=fontsize)
         elif 'Range Limit' == row[0] :
             range_limits = ( float(row[1]), float(row[2]) )
+            plt.text(0.05,fig_y,'%s: %s, %s' % (row[0],row[1],row[2]),fontsize=fontsize)
         elif 'SNR Limit' == row[0] :
             snr_limits = ( float(row[1]), float(row[2]) )
+            plt.text(0.05,fig_y,'%s: %s, %s' % (row[0],row[1],row[2]),fontsize=fontsize)
         elif 'Pass/Fail SNR' == row[0] :
             pass_fail_snr = float(row[1])
+            plt.text(0.05,fig_y,'%s: %s' % (row[0],row[1]),fontsize=fontsize)
+            fig_height = 1.0
+        elif 'TimeLimit' == row[0][:9] :
+            time_limit_minutes = int(row[1])
+            time_limit_seconds = time_limit_minutes * 60
+            plt.text(0.05,fig_y,'%s: %s' % (row[0],row[1]),fontsize=fontsize)
         elif 'SNR Bias' == row[0] :
             snr_bias_row = (int(row[1]),float(row[2]))
-            #snr_bias_row = [int(row[1]),float(row[2])]
             snr_bias_table.append(snr_bias_row)
-
-pii_array = np.array(pii_table,dtype=[('label','S10'),('low','f4'),('high','f4'),('color','S10')])
-snr_bias_array = np.array(snr_bias_table,dtype=[('nodeID',int),('snr_bias',float)])
-#pp(pii_array)
-#pp(range_limits)
-#pp(snr_limits)
+            plt.text(0.5,fig_y_snrbias,'%s: %s, %s' % (row[0],row[1],row[2]),fontsize=fontsize)
+            fig_y_snrbias -= 0.05
+        fig_y -= 0.05
 #pdb.set_trace()
 
-#------------------------- plot_ranges ----------------------------------------
-# def plot_ranges(RA,nodeID) :
-#
-#     nodeID_mask = RA['rspID'] == nodeID
-#
-#     t_vec = RA[nodeID_mask]['t_host']
-#     t0 = RA['t_host'][0]
-#     t_vec = t_vec - t0
-#
-#     r_vec = RA[nodeID_mask]['rmeas']
-#
-#     plt.plot(t_vec,r_vec,'b.')
-#     plt.grid(True)
+plt.tick_params(
+    axis='both',          # changes apply to the x-axis
+    which='both',      # both major and minor ticks are affected
+    bottom='off',      # ticks along the bottom edge are off
+    top='off',         # ticks along the top edge are off
+    labelbottom='off') # labels along the bottom edge are off
+fig.patch.set_visible(False)
+ax.axis('off')
+plt.draw()
+pdf.savefig()
 
-#------------------------- plot_snr ----------------------------------------
-# def plot_snr(RA,nodeID,color,low,high,label,pass_fail) :
-#
-#     nodeID_mask = RA['rspID'] == nodeID
-#
-#     mask_pii_hi = RA[nodeID_mask]['t_stopwatch'] >= low
-#     mask_pii_hi = mask_pii_hi.ravel()
-#
-#     mask_pii_low = RA[nodeID_mask]['t_stopwatch'] <= high
-#     mask_pii_low = mask_pii_low.ravel()
-#
-#     mask_pii = np.logical_and(mask_pii_hi,mask_pii_low)
-#
-#     t0 = RA['t_host'][0]
-#     t_vec = RA[nodeID_mask][mask_pii]['t_host'] - t0
-#     vpk_vec = RA[nodeID_mask][mask_pii]['vpeak']
-#     noise_vec = RA[nodeID_mask][mask_pii]['noise']
-#     snr_vec = 20.*np.log10(np.divide(vpk_vec,noise_vec))
-#
-#     lineH, = plt.plot(t_vec,snr_vec,color,
-#             hold=True, linestyle='None', marker='o', markersize=6,
-#             markerfacecolor=color, markeredgecolor=color, label=label)
-#     if t_vec.size > 0 :
-#         plt.plot([min(t_vec),max(t_vec)],[pass_fail,pass_fail],'k-')
-#     plt.grid(True)
-#
-#     return lineH
+pii_array = np.array(pii_table,dtype=[('label','S10'),('low','f4'),('high','f4'),('color','S10')])
+snr_bias_array = np.array(snr_bias_table,dtype=[('rspID',int),('snr_bias',float)])
 
-req_list = np.unique(D['RcmRanges']['reqID'])
-rsp_list = np.unique(D['RcmRanges']['rspID'])
-pii_list = np.unique(D['RcmRanges']['t_stopwatch'])
 
-fn_pdf = fn_main + '.pdf'
-ffn_pdf = os.path.join(path,fn_pdf)
-pdf = PdfPages(ffn_pdf)
+#----- Read logfile into range_array
+range_array = np.empty(1,
+            dtype = [('timestamp',float),('rspID',int),('stopwatch',int),
+                ('rmeas',float),('noise',int),('vpeak',int),('reqSNR',int)])
+t0 = []
+irow = 0
+with open(ffn_in,'rb') as csvfile :
+    stuff = csv.reader(csvfile)
+    for row in stuff :
+
+        irow += 1
+        if not irow % 1000 : # print a line once in a while
+           print '%d: %s' % (irow,row)
+
+        if row[0] == 'Timestamp' :
+            continue
+
+        if row[1] == ' RcmRangeInfo' :
+            timestamp = float(row[0])
+            if not t0 :
+                t0 = timestamp
+            if timestamp > t0 + time_limit_seconds :
+                break
+
+            rspID = int(row[3])
+            stopwatch = int(row[7])
+            rmeas = float(row[8])/1000.
+            noise = int(row[19])
+            vpeak = int(row[20])
+            reqSNR = int(row[23])
+            range_row = np.array([(timestamp,rspID,stopwatch,rmeas,noise,vpeak,reqSNR)],
+                dtype = [('timestamp',float),('rspID',int),('stopwatch',int),('rmeas',float),
+                ('noise',int),('vpeak',int),('reqSNR',int)])
+            #pdb.set_trace()
+            range_array = np.vstack((range_array,range_row))
+
+# First entry was empty.  Trim it out.
+range_array = range_array[1:]
+
+rsp_list = np.unique(range_array['rspID'])
+stopwatch_list = np.unique(range_array['stopwatch'])
 
 plt.ion()
 #rsp_list = [101,102]
-for nodeID in rsp_list :
+for rspID in rsp_list :
 
-    RA = D['RcmRanges'].ravel()
+    #RA = D['RcmRanges'].ravel()
 
     if 'fig_size' in locals() :
         fig = plt.figure(figsize=fig_size)
@@ -164,45 +187,44 @@ for nodeID in rsp_list :
         fig = plt.figure()
     #figsize_pix = fig.get_size_inches()*fig.dpi
 
-    #-------------- Plot Ranges ------------------------------------------------
+
+    #-------------- Plot Ranges for this rspID ---------------------------------
     ax0 = plt.subplot(311)
-    #plot_ranges(RA,nodeID)
-    nodeID_mask = RA['rspID'] == nodeID
-    t_vec = RA[nodeID_mask]['t_host']
-    t0 = RA['t_host'][0]
-    t_vec = t_vec - t0
-    r_vec = RA[nodeID_mask]['rmeas']
+
+    rspID_mask = range_array['rspID'] == rspID
+    t_vec = range_array[rspID_mask]['timestamp']
+    t_vec = t_vec - t_vec[0]
+    r_vec = range_array[rspID_mask]['rmeas']
     plt.plot(t_vec,r_vec,'b.')
     plt.grid(True)
     plt.suptitle(fn_main,fontsize=14)
-    plt.title('Responder: %d' % (nodeID),fontsize=14)
+    plt.title('Responder: %d' % (rspID),fontsize=14)
     plt.ylabel('Distance (m)',fontsize=14)
     if 'range_limits' in locals() :
         ax0.set_ylim(range_limits)
 
-    #-------------- Plot TX SNR ------------------------------------------------
+
+    #-------------- Plot TX SNR for this rspID -------------------------------
     ax1 = plt.subplot(312)
 
     lineH = []
     for iPii in range(1,len(pii_array)) :
-        nodeID_mask = RA['rspID'] == nodeID
-        mask_pii_hi = RA[nodeID_mask]['t_stopwatch'] >= pii_array[iPii]['low']
-        mask_pii_hi = mask_pii_hi.ravel()
-        mask_pii_low = RA[nodeID_mask]['t_stopwatch'] <= pii_array[iPii]['high']
-        mask_pii_low = mask_pii_low.ravel()
-        #mask_pii = np.logical_and(mask_pii_hi,mask_pii_low)
-        mask_pii = mask_pii_hi & mask_pii_low
 
-        t_vec = RA[nodeID_mask][mask_pii]['t_host'] - RA['t_host'][0]
-        vpk_vec = RA[nodeID_mask][mask_pii]['vpeak']
-        noise_vec = RA[nodeID_mask][mask_pii]['noise']
+        rspID_mask = range_array['rspID'] == rspID
+        pii_hi_mask = range_array[rspID_mask]['stopwatch'] >= pii_array[iPii]['low']
+        pii_low_mask = range_array[rspID_mask]['stopwatch'] <= pii_array[iPii]['high']
+        pii_mask = pii_hi_mask & pii_low_mask
+
+#        t_vec = RA[nodeID_mask][mask_pii]['t_host'] - RA['t_host'][0]
+        t_vec = range_array[rspID_mask][pii_mask]['timestamp'] - range_array['timestamp'][0]
+        vpk_vec = range_array[rspID_mask][pii_mask]['vpeak']
+        noise_vec = range_array[rspID_mask][pii_mask]['noise']
         snr_vec = 20.*np.log10(np.divide(vpk_vec,noise_vec))
 
-        snr_bias_index = np.where(snr_bias_array['nodeID']==nodeID)
+        snr_bias_index = np.where(snr_bias_array['rspID']==rspID)
         snr_bias = snr_bias_array[snr_bias_index]['snr_bias']
-        snr_vec = snr_vec + snr_bias
-
-#        pdb.set_trace()
+        if snr_bias :
+            snr_vec = snr_vec + snr_bias
 
         color = pii_array[iPii]['color']
         label = pii_array[iPii]['label']
@@ -214,9 +236,6 @@ for nodeID in rsp_list :
             plt.plot([min(t_vec),max(t_vec)],[pass_fail_snr,pass_fail_snr],'k-')
     plt.grid(True)
 
-    #lineH =     [plot_snr(RA, nodeID, pii_array[0]['color'], pii_array[0]['low'], pii_array[0]['high'], pii_array[0]['label'], pass_fail_snr)]
-    #for iPii in range(1,len(pii_array)) :
-    #    lineH.append(plot_snr(RA, nodeID, pii_array[iPii]['color'], pii_array[iPii]['low'], pii_array[iPii]['high'], pii_array[iPii]['label'], pass_fail_snr))
     plt.ylabel('DUT TX SNR (dB)',fontsize=14)
     plt.xlabel('Time (s)')
     plt.legend(bbox_to_anchor=(0.,1.03,1.,.102),
@@ -225,37 +244,28 @@ for nodeID in rsp_list :
         ax1.set_ylim(snr_limits)
 
 
-
-    # lineH =     [plot_snr(RA, nodeID, pii_array[0]['color'], pii_array[0]['low'], pii_array[0]['high'], pii_array[0]['label'], pass_fail_snr)]
-    # for iPii in range(1,len(pii_array)) :
-    #     lineH.append(plot_snr(RA, nodeID, pii_array[iPii]['color'], pii_array[iPii]['low'], pii_array[iPii]['high'], pii_array[iPii]['label'], pass_fail_snr))
-    # plt.ylabel('DUT TX SNR (dB)',fontsize=14)
-    # plt.xlabel('Time (s)')
-    # plt.legend(bbox_to_anchor=(0.,1.03,1.,.102),
-    #             mode='expand',ncol=len(lineH),fancybox=True,shadow=True)
-    # if 'snr_limits' in locals() :
-    #     ax1.set_ylim(snr_limits)
-
-
-    #-------------- Plot RX SNR ------------------------------------------------
+    #-------------- Plot RX SNR for this rspID ---------------------------------
     ax2 = plt.subplot(313)
 
     lineH = []
     for iPii in range(1,len(pii_array)) :
-        nodeID_mask = RA['rspID'] == nodeID
-        mask_pii_hi = RA[nodeID_mask]['t_stopwatch'] >= pii_array[iPii]['low']
-        mask_pii_hi = mask_pii_hi.ravel()
-        mask_pii_low = RA[nodeID_mask]['t_stopwatch'] <= pii_array[iPii]['high']
-        mask_pii_low = mask_pii_low.ravel()
+
+        rspID_mask = range_array['rspID'] == rspID
+        #nodeID_mask = RA['rspID'] == nodeID
+        pii_hi_mask = range_array[rspID_mask]['stopwatch'] >= pii_array[iPii]['low']
+        #mask_pii_hi = mask_pii_hi.ravel()
+        pii_low_mask = range_array[rspID_mask]['stopwatch'] <= pii_array[iPii]['high']
+        #mask_pii_low = mask_pii_low.ravel()
         #mask_pii = np.logical_and(mask_pii_hi,mask_pii_low)
-        mask_pii = mask_pii_hi & mask_pii_low
+        pii_mask = pii_hi_mask & pii_low_mask
 
-        t_vec = RA[nodeID_mask][mask_pii]['t_host'] - RA['t_host'][0]
-        snr_vec = RA[nodeID_mask][mask_pii]['reqSNR']
+        t_vec = range_array[rspID_mask][pii_mask]['timestamp'] - range_array['timestamp'][0]
+        snr_vec = range_array[rspID_mask][pii_mask]['reqSNR']
 
-        snr_bias_index = np.where(snr_bias_array['nodeID']==nodeID)
+        snr_bias_index = np.where(snr_bias_array['rspID'] == rspID)
         snr_bias = snr_bias_array[snr_bias_index]['snr_bias']
-        snr_vec = snr_vec + snr_bias
+        if snr_bias :
+            snr_vec = snr_vec + snr_bias
 
 #        pdb.set_trace()
 
